078d78aa289d34084129ceff76992f6e2cd97a5e
artwallace <1663489+artwallace@users.noreply.github.com>
2021-07-31 13:03:20 -0500

    add dist

---
+++ app/dist/precache-manifest.2b35e465cac01d0607f67301eddc59b8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ac917fc82bbf8f9a7449",
+    "url": "/css/app.a0b0f15f.css"
+  },
+  {
+    "revision": "ad9be5b1c60109a64f2756c30ec3afe9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "30509395adf935207e6c",
+    "url": "/js/about.5a474f6e.js"
+  },
+  {
+    "revision": "ac917fc82bbf8f9a7449",
+    "url": "/js/app.c19a6e9a.js"
+  },
+  {
+    "revision": "804691e5b42c54ab80a9",
+    "url": "/js/chunk-vendors.e5ae3a12.js"
+  },
+  {
+    "revision": "833d8182528a24c31b14f9eb0af10af2",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
